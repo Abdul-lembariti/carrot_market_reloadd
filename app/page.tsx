3b8928d920fd6875350513ce5848d0c8a294efc1
Abdul Lembariti <144220434+Abdul-lembariti@users.noreply.github.com>
2024-05-14 12:09:46 +0300

    skeleton making in tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-red-300 lg:bg-red-500">
-      <div className="bg-white shadow-lg p-5 rounded-xl w-full max-w-screen-sm flex flex-col gap-2 *:outline-none has-[:invalid]:bg-red-300">
-        <input
-          className="w-full rounded-full py-3 bg-gray-200 pl-5  focus:ring-green-300 focus:ring-2 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          placeholder="Enter email here "
-          required
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className="bg-gray-500 to-purple-300 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90  md:px-10 peer-required:bg-green-500">
-          Login
-        </button>
+    <main className="bg-gray-300 h-screen flex items-center justify-center p-3">
+      <div className="bg-white shadow-lg p-5 rounded-xl w-full max-w-screen-sm flex flex-col gap-3 ">
+        {['Nahul', 'Me', 'You', 'Yourselft'].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 p-2.5 rounded-lg odd:bg-gray-100 even:bg-cyan-100 border-b-2 pb-5 last:border-0 ">
+            <div className="size-10 bg-blue-300 rounded-full" />
+            <div className="w-40 h-3 rounded-full bg-gray-400 animate-pulse " />
+            <div className="w-20 h-3 rounded-full bg-gray-400 animate-pulse " />
+          </div>
+        ))}
       </div>
     </main>
   )
