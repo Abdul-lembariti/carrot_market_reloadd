1c3eba6830433d94240a0c986b24e939e65ff95b
Abdul Lembariti <144220434+Abdul-lembariti@users.noreply.github.com>
2024-05-14 12:16:50 +0300

    list and animation..

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-3">
-      <div className="bg-white shadow-lg p-5 rounded-xl w-full max-w-screen-sm flex flex-col gap-3 ">
-        {['Nahul', 'Me', 'You', 'Yourselft'].map((person, index) => (
+    <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-xl w-full max-w-screen-sm flex flex-col gap-4">
+        {['Nahul', 'Me', 'You', 'Yourself', ''].map((person, index) => (
           <div
             key={index}
-            className="flex items-center gap-5 p-2.5 rounded-lg odd:bg-gray-100 even:bg-cyan-100 border-b-2 pb-5 last:border-0 ">
+            className="flex items-center gap-5 p-2.5 rounded-lg odd:bg-gray-100 even:bg-cyan-100 border-b-2 pb-5 last:border-0 last:pb-0">
             <div className="size-10 bg-blue-300 rounded-full" />
-            <div className="w-40 h-3 rounded-full bg-gray-400 animate-pulse " />
-            <div className="w-20 h-3 rounded-full bg-gray-400 animate-pulse " />
+            <span className="text-lg font-medium empty:w-24 empty:h-3 empty:rounded-full empty:bg-gray-300 empty:animate-pulse">
+              {person}
+            </span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+            </div>
           </div>
         ))}
       </div>
