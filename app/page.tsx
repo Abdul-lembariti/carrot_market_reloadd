4a13d902c0a036f2732bb7a618f638f6c433f2e6
Abdul Lembariti <144220434+Abdul-lembariti@users.noreply.github.com>
2024-05-14 14:56:38 +0300

    #3 Tailwindcss compled

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,9 @@ export default function Home() {
   return (
     <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-xl w-full max-w-screen-sm flex flex-col gap-4">
-        {['Nahul', 'Me', 'You', 'Yourself', ''].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5 p-2.5 rounded-lg odd:bg-gray-100 even:bg-cyan-100 border-b-2 pb-5 last:border-0 last:pb-0 group">
-            <div className="size-10 bg-blue-300 rounded-full" />
-            <span className="text-lg font-medium empty:w-24 empty:h-3 empty:rounded-full empty:bg-gray-300 empty:animate-pulse group-hover:text-red-500">
-              {person}
-            </span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
-              <span className="z-10">{index}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-            </div>
-          </div>
-        ))}
+        <button className="w-full h-10 bg-black text-white rounded-full">
+          Submit
+        </button>
       </div>
     </main>
   )
