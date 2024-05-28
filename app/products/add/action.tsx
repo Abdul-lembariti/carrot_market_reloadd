2b5179a99f60795baec65e19585b83e443e2388f
Abdul Lembariti <144220434+Abdul-lembariti@users.noreply.github.com>
2024-05-28 17:23:26 +0300

    github..

--- app/products/add/action.tsx
+++ app/products/add/action.tsx
@@ -6,49 +6,47 @@ import getSession from '../../../lib/session'
 import { redirect } from 'next/navigation'
 import { productSchema } from './schema'
 
+
+
 export async function uploadProduct(_: any, formData: FormData) {
-  try {
-    const data = {
-      photo: formData.get('photo'),
-      title: formData.get('title'),
-      price: formData.get('price'),
-      description: formData.get('description'),
-    }
+  const data = {
+    photo: formData.get('photo'),
+    title: formData.get('title'),
+    price: formData.get('price'),
+    description: formData.get('description'),
+  }
+  
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer()
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData))
+    data.photo = `/${data.photo.name}`
+  }
 
-    if (data.photo instanceof File) {
-      const photoData = await data.photo.arrayBuffer()
-      await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData))
-      data.photo = `/${data.photo.name}`
-    }
-
-    const result = productSchema.safeParse(data)
-    if (!result.success) {
-      console.error(result.error.flatten())
-      return result.error.flatten()
-    } else {
-      const session = await getSession()
-      if (session.id) {
-        const product = await db.product.create({
-          data: {
-            title: result.data.title,
-            description: result.data.description,
-            price: result.data.price,
-            photo: result.data.photo,
-            user: {
-              connect: {
-                id: session.id,
-              },
+  const result = productSchema.safeParse(data)
+  if (!result.success) {
+    return result.error.flatten()
+  } else {
+    const session = await getSession()
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
             },
           },
-          select: {
-            id: true,
-          },
-        })
-        redirect(`/products/${product.id}`)
-      }
+        },
+        select: {
+          id: true,
+        },
+      })
+      redirect(`/products/${product.id}`)
+      // redirect('/products')
     }
-  } catch (error) {
-    console.error('Error uploading product:', error)
-    throw new Error('Failed to upload product')
   }
+  // console.log(data)
 }
